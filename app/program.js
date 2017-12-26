6a65e89f10735cb337b51988024f80fb9a186603
Your Name <you@example.com>
2017-12-26 19:32:56 +0100

    add files to project

---
+++ app/program.js
@@ -0,0 +1,27 @@
+var seconds = require('../modules/seconds');
+var OSInfo = require('../modules/OSInfo'); 
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch(instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            case '/sayhello':   
+                process.stdout.write('hello!\n'); 
+                break;
+            case '/getOSinfo':
+                getOSinfo();
+                break; 
+            case '/seconds':
+                seconds();     
+            default: 
+                process.stderr.write('Wrong instruction!\n');
+        }
+    }
+})
+
+
